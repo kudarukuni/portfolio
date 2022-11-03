eb75a1361afa5bcb7676fd0dad728e48b17c87c5
Kuda Rukuni <52784179+kudarukuni@users.noreply.github.com>
2022-11-03 14:44:09 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -41,20 +41,6 @@ function App() {
           <Route path="/resume" element={<Resume />} />
           <Route path="*" element={<Navigate to="/"/>} />
         </Routes>
-        <script type="text/javascript">
-      (function () {
-          var options = {
-              whatsapp: "+263782516263",
-              call_to_action: "Whatsapp Kuda",
-              button_color: "#FF6550",
-              position: "right",
-          };
-          var proto = 'https:', host = "getbutton.io", url = proto + '//static.' + host;
-          var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
-          s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
-          var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
-      })();
-    </script>
         <Footer />
       </div>
     </Router>

922ed9fd7ec1d5cb40c4b4424f6ac939f6226e0f
Kuda Rukuni <52784179+kudarukuni@users.noreply.github.com>
2022-11-03 13:47:57 +0200

    Update ProjectCards.js

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -9,7 +9,7 @@ function ProjectCards(props) {
     <Card className="project-card-view">
       <Card.Img variant="top" src={props.imgPath} alt="card-img" />
       <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
+        <Card.Title style={{ color: "purple" }}>{props.title}</Card.Title>
         <Card.Text style={{ textAlign: "left" }}>
           {props.description}
         </Card.Text>

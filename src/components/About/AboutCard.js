fa05034a9ed4e422fbc7095828f4d545d0991b81
Kuda Rukuni <52784179+kudarukuni@users.noreply.github.com>
2024-01-11 11:30:37 +0200

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,46 +8,46 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            <span className="purple">H</span>e<span className="purple">l</span>l<span className="purple">o</span> o<span className="purple">n</span>c<span className="purple">e</span> a<span className="purple">gain! Nice to see</span> y<span className="purple">o</span>u <span className="purple">m</span>a<span className="purple">d</span>e <span className="purple">i</span>t h<span className="purple">ere, as mentioned </span><span className="purple">e</span>a<span className="purple">r</span>l<span className="purple">i</span>e<span className="purple">r</span> m<span className="purple">y</span> na<span className="white">me is Kuda Rukuni, </span>
-            I<span className="purple">'</span>m <span className="purple">f</span>r<span className="purple">o</span>m <span className="purple">t</span>h<span className="purple">e</span> capital city of <span className="purple">Harare, in Zimbabwe. Life has its twists and turns. My experience</span> ranges across many spectrums. 
-            I’m fortunate to have a full-time <span className="purple">role as a Senior Technician working on a large Audio and</span> Visual Solutions Startup known as Netcast Systems.
+            <span>H</span>e<span>l</span>l<span>o</span> o<span>n</span>c<span>e</span> a<span>gain! Nice to see</span> y<span>o</span>u <span>m</span>a<span>d</span>e <span>i</span>t h<span>ere, as mentioned </span><span>e</span>a<span>r</span>l<span>i</span>e<span>r</span> m<span>y</span> na<span className="white">me is Kuda Rukuni, </span>
+            I<span>'</span>m <span>f</span>r<span>o</span>m <span>t</span>h<span>e</span> capital city of <span>Harare, in Zimbabwe. Life has its twists and turns. My experience</span> ranges across many spectrums. 
+            I’m fortunate to have a full-time <span>role as a Senior Technician working on a large Audio and</span> Visual Solutions Startup known as Netcast Systems.
             <br />
             <br />
-            I’ve been an <span className="purple">NFT</span> Collector for 3 years and a <span className="purple">Full-Stack</span> Web <span className="white">Developer</span> for 8 years. 
+            I’ve been an <span>NFT</span> Collector for 3 years and a <span>Full-Stack</span> Web <span className="white">Developer</span> for 8 years. 
             
             <br />
-            I’ve always had a passion for <span className="white">art, economics, music, aviation and design</span>. When I discovered the power of <span className="purple">Web Development</span>, it opened a world of endless possibilities <span className="white">&</span> opportunities. 
-            I <span className="purple">was</span> now able to finally blend my <span className="purple">skills</span> and passions into creating <span className="purple">awesome</span> projects that help the world. 
-            From front-end<span className="purple">/</span>back-end design, global macro analysis, to <span className="purple">blockchain</span> development and on-chain analytics.
+            I’ve always had a passion for <span className="white">art, economics, music, aviation and design</span>. When I discovered the power of <span>Web Development</span>, it opened a world of endless possibilities <span className="white">&</span> opportunities. 
+            I <span>was</span> now able to finally blend my <span>skills</span> and passions into creating <span>awesome</span> projects that help the world. 
+            From front-end<span>/</span>back-end design, global macro analysis, to <span>blockchain</span> development and on-chain analytics.
             <br />
             <br />
-            Apart from <span className="purple">coding</span>, some other <span className="purple">activities</span> that I <span className="purple">love</span> to do<span className="purple">!</span>
+            Apart from <span>coding</span>, some other <span>activities</span> that I <span>love</span> to do<span>!</span>
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Casual <span className="purple">C</span>h<span className="purple">e</span>s<span className="purple">s</span> Player
+              <ImPointRight /> Casual <span>C</span>h<span>e</span>s<span>s</span> Player
             </li>
             <li className="about-activity">
-              <ImPointRight /> Football <span className="purple">/</span> Soccer
+              <ImPointRight /> Football <span>/</span> Soccer
             </li>
             <li className="about-activity">
-              <ImPointRight /> Video <span className="purple">Games</span>
+              <ImPointRight /> Video <span>Games</span>
             </li>
            <li className="about-activity">
               <ImPointRight /> Travelling
             </li>
             <li className="about-activity">
-              <ImPointRight /> Trying <span className="purple">new</span> recipes
+              <ImPointRight /> Trying <span>new</span> recipes
             </li>
             <li className="about-activity">
-              <ImPointRight /> Learning <span className="purple">about</span> new <span className="purple">tech</span>
+              <ImPointRight /> Learning <span>about</span> new <span>tech</span>
             </li>
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
-            "Yesterday <span className="purple">I</span> was clever so <span className="purple">I</span> changed the world, today <span className="purple">I</span> am wise so <span className="purple">I</span> change myself"{" "}
+            "Yesterday <span>I</span> was clever so <span>I</span> changed the world, today <span>I</span> am wise so <span>I</span> change myself"{" "}
           </p>
-          <footer className="blockquote-footer"><span className="purple">Rumi</span></footer>
+          <footer className="blockquote-footer"><span>Rumi</span></footer>
         </blockquote>
       </Card.Body>
     </Card>

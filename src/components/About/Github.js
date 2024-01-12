338c6329469465e6acb4632549eb179151705e03
Kuda Rukuni <52784179+kudarukuni@users.noreply.github.com>
2024-01-12 13:50:19 +0200

    Update Github.js

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -6,14 +6,14 @@ function Github() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
       <h1 className="project-heading" style={{ paddingBottom: "20px" }}>
-        My Coding <strong className="purple">Statistics</strong>
+        <strong>My <font color="#27cbee">Coding</font> Statistics</strong>
       </h1>
       <GitHubCalendar
         username="kudarukuni"
         blockSize={15}
         blockMargin={5}
-        color="#c084f5"
-        fontSize={16}
+        color="#27cbee"
+        fontSize={14}
       />
     </Row>
   );

19f892a277eda5e286db3bb99db984b594ad3a1e
Kuda Rukuni <52784179+kudarukuni@users.noreply.github.com>
2024-01-12 17:04:27 +0200

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -25,7 +25,7 @@ function ResumeNew() {
         <Particle />
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
-            variant="primary"
+            variant="#27cbee"
             href={pdf}
             target="_blank"
             style={{ maxWidth: "250px" }}
@@ -44,7 +44,7 @@ function ResumeNew() {
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
-            variant="primary"
+            variant="#27cbee"
             href={pdf}
             target="_blank"
             style={{ maxWidth: "250px" }}

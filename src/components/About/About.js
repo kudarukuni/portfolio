153844cadacb382383c26c3bfab26f469ba6597f
Kuda Rukuni <52784179+kudarukuni@users.noreply.github.com>
2024-01-12 11:37:36 +0200

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -22,7 +22,7 @@ function About() {
             }}
           >
             <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
-              About <font color="#27cbee">Kuda</font>
+              About <p style={{ color: "rgb(39 203 239)" }}>Kuda</p>
             </h1>
             <Aboutcard />
           </Col>

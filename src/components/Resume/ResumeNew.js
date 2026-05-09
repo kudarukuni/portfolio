0b89ab6182d365a54f9a437a5395300502e71ce9
Kuda Rukuni <52784179+kudarukuni@users.noreply.github.com>
2026-05-09 10:50:23 +0200

    Update ResumeNew.js
    done renaming

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/kuda_rukuni_resume.pdf";
+import pdf from "../../Assets/../Assets/RESUME WILLIAM KUDAKWASHE RUKUNI.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";

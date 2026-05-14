4d823d7efb6ad2b0b7f3f8068b300ea8bfb2ba5f
Kuda Rukuni <52784179+kudarukuni@users.noreply.github.com>
2026-05-14 16:24:04 +0200

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -10,7 +10,7 @@ function AboutCard() {
           <p style={{ textAlign: "justify" }}>
             <span>H</span>e<span>l</span>l<span>o</span> o<span>n</span>c<span>e</span> a<span>gain! Nice to see</span> y<span>o</span>u <span>m</span>a<span>d</span>e <span>i</span>t h<span>ere, as mentioned </span><span>e</span>a<span>r</span>l<span>i</span>e<span>r</span> m<span>y</span> na<span className="white">me is Kuda Rukuni, </span>
             I<span>'</span>m <span>f</span>r<span>o</span>m <span>t</span>h<span>e</span> capital city of <span>Zimbabwe, Harare. Life has its twists and turns and some of my experiences</span> ranges across many spectrums. <br/><br/><font color="#27cbee">Currently I am working for ZESA's Subsidiary ZETDC located at No. 25 Samora Machel Ave, ZESA Holdings Head Office. My role at the company is post graduate systems analyst and software engineer trainee.
-            I’m fortunate to have a full-time <span>role as a Senior Technician working on a small Audio and</span> Visual Solutions Startup known as <a href="https://www.netcast.co.zw" target="_blank">Netcast Systems</a>.</font>
+            I’m fortunate to have a full-time <span>role as a Senior Technician working on a small Audio and</span> Visual Solutions Startup known as <a href="https://www.netcast.co.zw" target="_blank" rel="noreferrer">Netcast Systems</a>.</font>
             <br />
             <br />
             I’ve been an <span>NFT</span> Collector for 3 years and a <span>Full-Stack</span> Web <span className="white">Developer</span> for 8 years. 

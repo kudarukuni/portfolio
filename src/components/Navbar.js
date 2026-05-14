d51854449d885691f9cf8f9d804a78d606e1227a
Kuda Rukuni <52784179+kudarukuni@users.noreply.github.com>
2026-05-14 16:29:07 +0200

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ import logo from "../Assets/favicon.png";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import { CgGitFork } from "react-icons/cg";
-import { ImBlog } from "react-icons/im";
+//import { ImBlog } from "react-icons/im";
 import {
   AiFillGithub,
   AiOutlineHome,

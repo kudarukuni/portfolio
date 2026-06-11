5e88b904bf590b64c8d4c2ead1637440448a2d53
kudarukuni <krukuni@gmail.com>
2026-06-11 16:28:23 +0200

    homemainsvg

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import homeLogo from "../../Assets/logo4.png";
+import homeLogo from "../../Assets/home-main.svg";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
